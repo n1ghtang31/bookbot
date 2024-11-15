6da6d68fd20fee960edbc1a6968032f8b237e30e
n1ghtang31 <crossiter0@gmail.com>
2024-11-15 14:43:59 -0600

    added sort and print report

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ def main():
     book_path = 'books/frankenstein.txt'
     text = book_text(book_path)
     num_words = count_words(text)
-    num_characters_total = count_characters(text)
-    num_characters = count_characters_total(text)
-    print(f'Words: {num_words}')
-    print(f'Total Characters: {num_characters_total}')
-    print(f'Characters: {num_characters}')
+    num_characters = count_characters(text)
+    #num_characters_total = count_characters_total(text)
+    num_characters_sorted = character_sort(num_characters)
+    print_report(num_words, num_characters_sorted, book_path)
+
+    
 
 def book_text(path):
     with open(path) as f:
@@ -32,4 +33,24 @@ def count_characters(file_contents):
                 count[char] = 1
 
     return count
+
+
+def character_sort(dict):
+    sorted_key = sorted(dict.keys())
+    sorted_dict = {key: dict[key] for key in sorted_key}
+    return sorted_dict
+
+
+
+
+
+def print_report(words, characters, path):
+    print(f"--- Begin report of {path} ---")   
+    print(f"{words} words found in the document") 
+    for char, count in characters.items():
+        print(f"The '{char}' character was found {count} times")
+
+    print(f"--- End report ---")
+
+
 main()
