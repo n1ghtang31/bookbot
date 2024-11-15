1f7e5b0ec079134286228af6657b629b4bdc3797
Craig Rossiter <crossiter0@gmail.com>
2024-11-15 07:56:09 -0600

    added ability to read file with printing text and word count

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+def main():
+    book_path = 'books/frankenstein.txt'
+    text = book_text(book_path)
+    num_words = count_words(text)
+    print(num_words)
+
+
+def book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+        #print(file_contents)
+        return file_contents
+
+def count_words(file_contents):
+        words = file_contents.split()
+        return len(words)
+
+main()
